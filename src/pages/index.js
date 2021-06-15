a79f791e6da2004107e0854ca4882618425c325c
Adarsh Gupta <adarshkgupta@outlook.com>
2021-06-15 17:02:31 +0530

    some touches #4

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,7 @@ import UltimateFeatures from 'sections/ultimate-features';
 import CustomerSupport from 'sections/customer-support';
 import Pricing from 'sections/pricing-flawed';
 import Support from 'sections/support';
-import Clients from 'sections/clients';
-import Blog from 'sections/blog';
+import Clients from 'sections/clients';;
 import Faq from 'sections/faq';
 import CallToAction from 'components/footer/calltoaction';
 
